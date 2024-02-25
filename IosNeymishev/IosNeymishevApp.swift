a897718902b4ca879d8e0893fc750d3f908af645
Harned <aleshkindima25@gmail.com>
2024-02-25 19:29:25 +0600

    Initial Commit

---
+++ IosNeymishev/IosNeymishevApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosNeymishevApp.swift
+//  IosNeymishev
+//
+//  Created by Nes on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct IosNeymishevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
